eaffcf4ca3a6e21c470016135859f82fbfd52d27
Alberto <albertocristian@gmail.com>
2020-01-25 10:30:56 -0300

    Primeiro commit

---
+++ android/app/src/main/kotlin/br/com/sicomwebteam/n2b_social_media/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.sicomwebteam.n2b_social_media
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
